4204736606f9ecba6352e810d1d49505b6484c5d
Dan-bsk <bosnjak.daniel1994@gmail.com>
2025-07-02 15:35:22 +0200

    add sanity files

---
+++ src/sanity/schemaTypes/types/author.ts
@@ -0,0 +1,40 @@
+// src/types/author.ts
+
+export interface Author {
+  _id: string
+  _type: 'author'
+  name: string
+  slug?: {
+    _type: 'slug'
+    current: string
+  }
+  image?: {
+    _type: 'image'
+    asset: {
+      _ref: string
+      _type: 'reference'
+    }
+    hotspot?: {
+      x: number
+      y: number
+      height: number
+      width: number
+    }
+  }
+  bio?: PortableTextBlock[]
+}
+
+// Portable Text Block, stark vereinfacht (für den "block" Typ)
+export interface PortableTextBlock {
+  _key?: string
+  _type: 'block'
+  style?: string
+  children: Array<{
+    _key?: string
+    _type: 'span'
+    text: string
+    marks?: string[]
+  }>
+  markDefs?: any[]
+  listItem?: string
+}
---
+++ src/sanity/schemaTypes/types/category.ts
@@ -0,0 +1,12 @@
+// src/types/category.ts
+
+export interface Category {
+  _id: string
+  _type: 'category'
+  title: string
+  slug?: {
+    _type: 'slug'
+    current: string
+  }
+  description?: string
+}
